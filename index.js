c85370b72afe6387eb3c1b70ab78f1c8a3f0a1bf
EC2 Default User <ec2-user@ip-172-31-45-198.us-east-2.compute.internal>
2020-04-20 03:07:49 +0000

    lab9

--- index.js
+++ index.js
@@ -59,5 +59,5 @@ app.get('/author/:aid', function(req, res){
 /* Start the application server */
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, function(){
-    console.log('Server has been started');
+    console.log('Server is running on port ${PORT}.');
 })
